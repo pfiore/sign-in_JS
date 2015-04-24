7e9e4393654b10d7d751a366fdbaddd704386090
patrick <student@epicodus.com>
2015-04-24 09:42:07 -0700

    creates Students Controller

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -2,39 +2,16 @@ studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsF
   $scope.StudentsFactory = StudentsFactory;
   $scope.students = StudentsFactory.students;
 
-
-  $scope.addStudent = function() {
-    var name = $scope.studentName;
-    $scope.StudentsFactory.addStudent(name);
-    $scope.studentName = null;
+  $scope.addSignIn = function(student) {
+    $scope.StudentsFactory.addSignIn(student);
   };
 
-  $scope.deleteStudent = function(student) {
-    $scope.StudentsFactory.deleteStudent(student);
+  $scope.getSignIn = function(student) {
+    return $scope.StudentsFactory.getSignIn(student);
   };
 
-  $scope.addPermission = function(student) {
-    $scope.StudentsFactory.addPermission(student);
+  $scope.changeSignIn = function(student) {
+    return $scope.StudentsFactory.changeSignIn(student);
   };
 
-  $scope.getPermission = function(student) {
-    return $scope.StudentsFactory.getPermission(student);
-  };
-
-  $scope.changePermission = function(student) {
-    return $scope.StudentsFactory.changePermission(student);
-  };
-
-  $scope.sortColumn = function(clickedColumn) {
-    if (!$scope.column || ($scope.column[0] === '-' && $scope.column.substr(1) === clickedColumn) || $scope.column != clickedColumn) {
-      $scope.column = clickedColumn;
-    } else {
-      $scope.column = '-' + clickedColumn;
-    }
-  };
-
-  $scope.startsWith = function (query, expected) {
-    var lowerStr = (query + "").toLowerCase();
-    return lowerStr.indexOf(expected.toLowerCase()) === 0;
-  };
 });
