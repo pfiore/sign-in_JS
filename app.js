60d1e15e7e07a9a92788ee9d02fb1c19c49aff89
Patrick <pfiorentino@Patricks-MacBook-Air.local>
2015-04-24 14:01:49 -0700

    creates route to home page in app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var studentRoster = angular.module('studentRoster', []);
+var studentSignIn = angular.module('studentRoster', ['ui.router']);
+
+studentSignIn.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url:"",
+    templateUrl: "patrials/home.html"
+  });
+
+});
